61591ba334db2f9e427bb1234f0a607e49570316
J0NL1N <jl8918@nyu.edu>
2020-12-02 00:15:12 -0500

    Configure firebase connection

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import firebase from "firebase/app";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
@@ -9,6 +10,20 @@ import Navigation from "./navigation";
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBJkgJx3TVuiXePkpkibGTUidmIbDF82lI",
+    authDomain: "cookit-714f6.firebaseapp.com",
+    databaseURL: "https://cookit-714f6.firebaseio.com",
+    projectId: "cookit-714f6",
+    storageBucket: "cookit-714f6.appspot.com",
+    messagingSenderId: "195744665965",
+    appId: "1:195744665965:web:f23eda55ee447a978aca64",
+    measurementId: "G-PVVKQWPRCL",
+  };
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
 
   if (!isLoadingComplete) {
     return null;
